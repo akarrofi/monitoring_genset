34fe0901283c6237dd6829d97aa59e5043fde1af
akarrofi <bhie.arr@gmail.com>
2026-06-08 11:20:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+st.title("Sistem Monitoring Genset")
+st.write("Selamat datang! Silakan login terlebih dahulu.")
+
+user = st.text_input("ID Pegawai")
+pwd = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if user == "admin" and pwd == "12345":
+        st.success("Berhasil Login!")
+        st.balloons()
+    else:
+        st.error("ID atau Password salah.")
